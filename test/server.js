57c3a4ea92b2c3aac580805ca69d3f01c2caeb53
Pixels & Bytes <pxandbytes@gmail.com>
2013-07-29 23:54:41 -0700

    Fix Server.run tests

--- test/server.js
+++ test/server.js
@@ -4,215 +4,223 @@ var h2o = require('../lib/server.js'),
 	http = require('http');
 
 /* global describe, it, after */
-describe('#setAppDefiner', function() {
+describe('Server', function() {
 	'use strict';
 
-	describe('Given a function that accepts 1 parameter', function() {
-		it('should pass', function() {
-			var testFunc = function() {
-				/* jshint unused:false */
-				h2o().setAppDefiner(function(param) {});
-			};
-			testFunc.should.not.throwError();
+	describe('#setAppDefiner', function() {
+
+		describe('Given a function that accepts 1 parameter', function() {
+			it('should pass', function() {
+				var testFunc = function() {
+					/* jshint unused:false */
+					h2o().setAppDefiner(function(param) {});
+				};
+				testFunc.should.not.throwError();
+			});
 		});
+
+		describe('Given a function that accepts 2 parameters', function() {
+			it('should pass', function() {
+				var testFunc = function() {
+					/* jshint unused:false */
+					h2o().setAppDefiner(function(a, b) {});
+				};
+				testFunc.should.not.throwError();
+			});
+		});
+
+		describe('Given a function that accepts no parameters', function() {
+			it('should throw error', function() {
+				var testFunc = function() {
+					h2o().setAppDefiner(function() {});
+				};
+				testFunc.should.throwError();
+			});
+		});
+
+		describe('Given a value', function() {
+			it('should throw error', function() {
+				var testFunc = function() {
+					h2o().setAppDefiner('not a function');
+				};
+				testFunc.should.throwError();
+			});
+		});
+
 	});
 
-	describe('Given a function that accepts 2 parameters', function() {
-		it('should pass', function() {
-			var testFunc = function() {
-				/* jshint unused:false */
-				h2o().setAppDefiner(function(a, b) {});
-			};
-			testFunc.should.not.throwError();
+	describe('#setLogger', function() {
+
+		describe('Given an object with info(), warn(), error(), and fatal(); each accept 1 parameter', function() {
+			it('should pass', function() {
+				var testFunc = function() {
+					/* jshint unused:false */
+					h2o().setLogger({
+						info: function(msg) {},
+						warn: function(msg) {},
+						error: function(msg) {},
+						fatal: function(msg) {}
+					});
+				};
+				testFunc.should.not.throwError();
+			});
 		});
+
+		describe('Given an object with info(), warn(), error(), and fatal(), with other properties', function() {
+			it('should pass', function() {
+				var testFunc = function() {
+					/* jshint unused:false */
+					h2o().setLogger({
+						info: function(msg) {},
+						warn: function(msg) {},
+						error: function(msg) {},
+						fatal: function(msg) {},
+						extra: 'irrelevant'
+					});
+				};
+				testFunc.should.not.throwError();
+			});
+		});
+
+		describe('Given an object with info(), warn(), error(), and fatal(); with wrong # parameters', function() {
+			it('should throw error', function() {
+				var testFunc = function() {
+					/* jshint unused:false */
+					h2o().setLogger({
+						info: function(msg) {},
+						warn: function(msg) {},
+						error: function(msg) {},
+						fatal: function() {}
+					});
+				};
+				testFunc.should.throwError();
+			});
+		});
+
+		describe('Given an object with info(), warn(), error(), and fatal', function() {
+			it('should throw error', function() {
+				var testFunc = function() {
+					/* jshint unused:false */
+					h2o().setLogger({
+						info: function(msg) {},
+						warn: function(msg) {},
+						error: function(msg) {},
+						fatal: 'not a function'
+					});
+				};
+				testFunc.should.throwError();
+			});
+		});
+
+		describe('Given an object with info(), warn(), and error() only', function() {
+			it('should throw error', function() {
+				var testFunc = function() {
+					/* jshint unused:false */
+					h2o().setLogger({
+						info: function(msg) {},
+						warn: function(msg) {},
+						error: function(msg) {}
+					});
+				};
+				testFunc.should.throwError();
+			});
+		});
+
+		describe('Given a function (i.e. non-object)', function() {
+			it('should throw error', function() {
+				var testFunc = function() {
+					/* jshint unused:false */
+					h2o().setLogger(function() {});
+				};
+				testFunc.should.throwError();
+			});
+		});
+
 	});
 
-	describe('Given a function that accepts no parameters', function() {
-		it('should throw error', function() {
-			var testFunc = function() {
-				h2o().setAppDefiner(function() {});
-			};
-			testFunc.should.throwError();
+	describe('#setErrorHandler', function() {
+
+		describe('Given a function that accepts 4 parameters', function() {
+			it('should pass', function() {
+				var testFunc = function() {
+					/* jshint unused:false */
+					h2o().setErrorHandler(function(a, b, c, d) {});
+				};
+				testFunc.should.not.throwError();
+			});
 		});
+
+		describe('Given a function that accepts 5 parameters', function() {
+			it('should pass', function() {
+				var testFunc = function() {
+					/* jshint unused:false */
+					h2o().setErrorHandler(function(a, b, c, d, e) {});
+				};
+				testFunc.should.not.throwError();
+			});
+		});
+
+		describe('Given a function that accepts 3 parameters', function() {
+			it('should throw error', function() {
+				var testFunc = function() {
+					/* jshint unused:false */
+					h2o().setErrorHandler(function(a, b, c) {});
+				};
+				testFunc.should.throwError();
+			});
+		});
+
+		describe('Given a value', function() {
+			it('should throw error', function() {
+				var testFunc = function() {
+					h2o().setErrorHandler('not a function');
+				};
+				testFunc.should.throwError();
+			});
+		});
+
 	});
 
-	describe('Given a value', function() {
-		it('should throw error', function() {
-			var testFunc = function() {
-				h2o().setAppDefiner('not a function');
-			};
-			testFunc.should.throwError();
-		});
-	});
-});
+	describe('#run', function() {
 
-describe('#setLogger', function() {
-	'use strict';
-
-	describe('Given an object with info(), warn(), error(), and fatal(); each accept 1 parameter', function() {
-		it('should pass', function() {
-			var testFunc = function() {
-				/* jshint unused:false */
-				h2o().setLogger({
+		function makeServer() {
+			/* jshint unused:false */
+			return h2o()
+				.setAppDefiner(function(param) {})
+				.setLogger({
 					info: function(msg) {},
 					warn: function(msg) {},
 					error: function(msg) {},
 					fatal: function(msg) {}
-				});
-			};
-			testFunc.should.not.throwError();
+				})
+				.setErrorHandler(function(a, b, c, d) {});
+		}
+
+		describe('With cluster', function() {
+			it('should return a Cluster', function() {
+				var result = makeServer()
+					.setPort(8888)
+					.run();
+				result.should.be.an.instanceOf(Cluster);
+			});
 		});
+
+		describe('Without cluster', function() {
+			var result;
+
+			it('should return an HTTP server', function() {
+				result = makeServer()
+					.setPort(8889)
+					.setClusterUse(false)
+					.run();
+				result.should.be.an.instanceOf(http.Server);
+			});
+
+			after(function() {
+				result.close();
+			});
+		});
+
 	});
 
-	describe('Given an object with info(), warn(), error(), and fatal(), with other properties', function() {
-		it('should pass', function() {
-			var testFunc = function() {
-				/* jshint unused:false */
-				h2o().setLogger({
-					info: function(msg) {},
-					warn: function(msg) {},
-					error: function(msg) {},
-					fatal: function(msg) {},
-					extra: 'irrelevant'
-				});
-			};
-			testFunc.should.not.throwError();
-		});
-	});
-
-	describe('Given an object with info(), warn(), error(), and fatal(); some do not accept 1 parameter', function() {
-		it('should throw error', function() {
-			var testFunc = function() {
-				/* jshint unused:false */
-				h2o().setLogger({
-					info: function(msg) {},
-					warn: function(msg) {},
-					error: function(msg) {},
-					fatal: function() {}
-				});
-			};
-			testFunc.should.throwError();
-		});
-	});
-
-	describe('Given an object with info(), warn(), error(), and fatal', function() {
-		it('should throw error', function() {
-			var testFunc = function() {
-				/* jshint unused:false */
-				h2o().setLogger({
-					info: function(msg) {},
-					warn: function(msg) {},
-					error: function(msg) {},
-					fatal: 'not a function'
-				});
-			};
-			testFunc.should.throwError();
-		});
-	});
-
-	describe('Given an object with info(), warn(), and error() only', function() {
-		it('should throw error', function() {
-			var testFunc = function() {
-				/* jshint unused:false */
-				h2o().setLogger({
-					info: function(msg) {},
-					warn: function(msg) {},
-					error: function(msg) {}
-				});
-			};
-			testFunc.should.throwError();
-		});
-	});
-
-	describe('Given a function (i.e. non-object)', function() {
-		it('should throw error', function() {
-			var testFunc = function() {
-				/* jshint unused:false */
-				h2o().setLogger(function() {});
-			};
-			testFunc.should.throwError();
-		});
-	});
-});
-
-describe('#setErrorHandler', function() {
-	'use strict';
-
-	describe('Given a function that accepts 4 parameters', function() {
-		it('should pass', function() {
-			var testFunc = function() {
-				/* jshint unused:false */
-				h2o().setErrorHandler(function(a, b, c, d) {});
-			};
-			testFunc.should.not.throwError();
-		});
-	});
-
-	describe('Given a function that accepts 5 parameters', function() {
-		it('should pass', function() {
-			var testFunc = function() {
-				/* jshint unused:false */
-				h2o().setErrorHandler(function(a, b, c, d, e) {});
-			};
-			testFunc.should.not.throwError();
-		});
-	});
-
-	describe('Given a function that accepts 3 parameters', function() {
-		it('should throw error', function() {
-			var testFunc = function() {
-				/* jshint unused:false */
-				h2o().setErrorHandler(function(a, b, c) {});
-			};
-			testFunc.should.throwError();
-		});
-	});
-
-	describe('Given a value', function() {
-		it('should throw error', function() {
-			var testFunc = function() {
-				h2o().setErrorHandler('not a function');
-			};
-			testFunc.should.throwError();
-		});
-	});
-});
-
-describe('#run', function() {
-	'use strict';
-
-	function makeServer() {
-		/* jshint unused:false */
-		return h2o()
-			.setAppDefiner(function(param) {})
-			.setLogger({
-				info: function(msg) {},
-				warn: function(msg) {},
-				error: function(msg) {},
-				fatal: function(msg) {}
-			})
-			.setErrorHandler(function(a, b, c, d) {});
-	}
-
-	describe('With cluster', function() {
-		it('should return a Cluster', function() {
-			var result = makeServer().run();
-			result.should.be.an.instanceOf(Cluster);
-		});
-	});
-
-	describe('Without cluster', function() {
-		var result;
-
-		it('should return an HTTP server', function() {
-			result = makeServer()
-				.setClusterUse(false)
-				.run();
-			result.should.be.an.instanceOf(http.Server);
-		});
-
-		after(function() {
-			result.close();
-		});
-	});
 });
